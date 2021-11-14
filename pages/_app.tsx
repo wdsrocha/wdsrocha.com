2e9e99f55ef406a61a758edfaa94c3af09f4ae5c
Wesley Rocha <wesleysr1997@gmail.com>
2021-11-14 12:54:22 -0400

    bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,77 @@ import type { AppProps } from "next/app";
 import React from "react";
 import Link from "next/link";
 import { Layout } from "../components/Layout";
+import { createGlobalStyle, css } from "styled-components";
+
+// `css` is used due to a Prettier bug when used with `createGlobalStyle`
+// https://github.com/styled-components/vscode-styled-components/issues/175
+const GlobalStyle = createGlobalStyle`${css`
+  /* CSS Reset: https://courses.joshwcomeau.com/css-for-js/treasure-trove/010-global-styles */
+
+  /* 1. Use a more-intuitive box-sizing model. */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  /* 2. Remove default margin for common elements */
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  figure,
+  blockquote,
+  ul,
+  ol,
+  dl,
+  dt,
+  dd {
+    margin: 0;
+  }
+
+  /* 3. Allow percentage-based heights in the application */
+  html,
+  body,
+  #__next {
+    height: 100%;
+  }
+
+  /* 4. Improve the typography across the site. */
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  /* 5. Make images easier to work with */
+  img,
+  picture {
+    max-width: 100%;
+    display: block;
+  }
+
+  /* 6. Inherit fonts for inputs and buttons */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  /* 7. Create a root stacking context */
+  #__next {
+    isolation: isolate;
+  }
+`}`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
+import styled from "styled-components";
 import { Post, getAllPosts } from "../lib/api";
 
 export const getStaticProps: GetStaticProps<{ posts: Post[] }> = async () => {
@@ -13,6 +14,31 @@ export const getStaticProps: GetStaticProps<{ posts: Post[] }> = async () => {
     },
   };
 };
+
+const Main = styled.main`
+  display: inline-block;
+  position: relative;
+  color: white;
+
+  &:after {
+    content: "";
+    content: "";
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 1;
+    top: 110%;
+    left: 0;
+    backgroundcolor: white;
+    transformorigin: bottom right;
+    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+  }
+`;
 
 const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   posts,
