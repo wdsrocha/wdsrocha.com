b10041bc2e141806646e68c680af85dd80c2a7cf
Wesley Rocha <wesleysr1997@gmail.com>
2023-03-02 11:07:19 -0400

    Install Tina CMS

---
+++ .tina/config.ts
@@ -0,0 +1,74 @@
+import { defineConfig } from "tinacms";
+
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        label: "TIL Posts",
+        name: "til",
+        path: "contents/til",
+        ui: {
+          router: ({ document }) => {
+            return `/til/${document._sys.filename}`;
+          },
+          filename: {
+            slugify: (values) => {
+              return (values?.title ?? "")?.toLowerCase().replace(/ /g, "-");
+            },
+          },
+          defaultItem: () => ({
+            date: new Date().toISOString(),
+          }),
+        },
+        fields: [
+          {
+            type: "string",
+            label: "Title",
+            name: "title",
+            required: true,
+            isTitle: true,
+          },
+          {
+            type: "string",
+            label: "Description",
+            name: "description",
+            description: "Brief description used in SEO and RSS",
+            ui: {
+              component: "textarea",
+            },
+          },
+          {
+            type: "datetime",
+            required: true,
+            name: "date",
+            label: "Created at",
+          },
+          {
+            type: "string",
+            label: "Content",
+            name: "body",
+            isBody: true,
+            ui: {
+              component: "textarea",
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
