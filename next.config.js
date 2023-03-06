5a762d40789b91d032a5a238809c2b82d42f688a
Wesley Rocha <wesleysr1997@gmail.com>
2023-03-06 09:59:40 -0400

    Allow images from Tina domain

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+  images: {
+    domains: ['assets.tina.io'],
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
