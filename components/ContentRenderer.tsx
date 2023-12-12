3c8c9abdef52560ba1b9226bb3d7b9d37fb301c9
Wesley Rocha <wesleysr1997@gmail.com>
2023-12-12 06:10:42 -0400

    Fix long words in ContentRenderer breaking page layout

--- components/ContentRenderer.tsx
+++ components/ContentRenderer.tsx
@@ -7,11 +7,11 @@ interface Props {
 export const ContentRenderer = ({ children }: Props) => (
   <article
     className={classNames(
-      "max-w-none",
+      "max-w-none break-words",
       "prose prose-mauve sm:prose-xl",
       "prose-a prose-a:text-primary-11 prose-a:underline-offset-4 hover:prose-a:no-underline",
-      "prose-h1 prose-h1:break-words",
-      "prose-h2 prose-h2:break-words",
+      "prose-h1",
+      "prose-h2",
       "prose-code prose-code:rounded prose-code:bg-primary-3 prose-code:px-1 prose-code:text-primary-11 prose-code:before:content-[''] prose-code:after:content-['']"
     )}
     dangerouslySetInnerHTML={{ __html: (children as string) ?? "" }}
